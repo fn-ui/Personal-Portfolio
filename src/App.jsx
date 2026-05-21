1b6387d7bba00cc2aea4125547d0622c0c00709c
Fn-ui <fn0740839@gmail.com>
2026-05-21 13:44:24 +0300

    Fix component import casing

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,12 @@ import Navbar from "./components/Navbar"
 import Hero from "./components/Hero"
 import About from "./components/About"
 import Skills from "./components/Myskills"
-import Services from "./components/Services";
+import Services from "./components/services";
 import Projects from "./components/Projects"
 import Testimonials from "./components/Testimonials"
 import Contact from "./components/Contact"
 import Footer from "./components/Footer"
-import ChatBot from "./components/ChatBot"
+import ChatBot from "./components/chatbot"
 
 
 function App() {
