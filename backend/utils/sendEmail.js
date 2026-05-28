0137f2cd6cf64a0302d8db83f5a9bde6f0dc2f01
Fn-ui <fn0740839@gmail.com>
2026-05-28 11:44:52 +0300

    Force IPv4 for Gmail SMTP

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -11,9 +11,9 @@ const sendEmail = async ({ to, subject, text }) => {
         pass: process.env.EMAIL_PASS,
       },
 
+      family: 4, // FORCE IPv4
+
       connectionTimeout: 10000,
-      greetingTimeout: 10000,
-      socketTimeout: 10000,
     });
 
     await transporter.sendMail({
