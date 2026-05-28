936d3ca654a9f4d4006460494a28a7d4d6829a5a
Fn-ui <fn0740839@gmail.com>
2026-05-28 11:33:33 +0300

    Improve email handling for Render

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -2,17 +2,18 @@ const nodemailer = require("nodemailer");
 
 const sendEmail = async ({ to, subject, text }) => {
   try {
+
     const transporter = nodemailer.createTransport({
-      host: "smtp.gmail.com",
-
-      port: 587,
-
-      secure: false,
+      service: "gmail",
 
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
       },
+
+      connectionTimeout: 10000,
+      greetingTimeout: 10000,
+      socketTimeout: 10000,
     });
 
     await transporter.sendMail({
