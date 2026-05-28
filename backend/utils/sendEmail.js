cc3744b93c7120278c0170588486d09659dcde82
Fn-ui <fn0740839@gmail.com>
2026-05-28 11:54:11 +0300

    Force IPv4 Gmail SMTP

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -1,17 +1,28 @@
 const nodemailer = require("nodemailer");
+const dns = require("dns");
+
+dns.setDefaultResultOrder("ipv4first");
 
 const sendEmail = async ({ to, subject, text }) => {
   try {
 
     const transporter = nodemailer.createTransport({
-      service: "gmail",
+      host: "smtp.gmail.com",
+
+      port: 587,
+
+      secure: false,
+
+      family: 4,
 
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
       },
 
-      family: 4, // FORCE IPv4
+      tls: {
+        rejectUnauthorized: false,
+      },
 
       connectionTimeout: 10000,
     });
